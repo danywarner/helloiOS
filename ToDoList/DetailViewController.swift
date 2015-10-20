eb6e7fdd1353e3c0b878f0ff817bca2597ffc9f0
Daniel Warner <danywarner@gmail.com>
2015-10-20 10:43:23 -0500

    local notifications

--- ToDoList/DetailViewController.swift
+++ ToDoList/DetailViewController.swift
@@ -16,6 +16,24 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var dateLabel: UILabel!
     
+    @IBAction func addNotification(sender: UIBarButtonItem) {
+        if let dateString = self.dateLabel.text {
+            if let date = parseDate(dateString) {
+                scheduleNotification(self.item!, date: date)
+            }
+        }
+    }
+    
+    func scheduleNotification(message: String, date: NSDate){
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = date
+        localNotification.timeZone = NSTimeZone.defaultTimeZone()
+        localNotification.alertBody = message
+        localNotification.alertTitle = "Recuerda esta tarea!"
+        localNotification.applicationIconBadgeNumber = 1
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("item \(item)")
@@ -24,7 +42,7 @@ class DetailViewController: UIViewController {
     }
 
     @IBAction func dateSelected(sender: UIDatePicker) {
-        print("fecha seleccionada \(sender.date)")
+        //print("fecha seleccionada \(sender.date)")
         self.dateLabel.text = formatDate(sender.date)
     }
     
@@ -32,6 +50,12 @@ class DetailViewController: UIViewController {
         let formatter = NSDateFormatter()
         formatter.dateFormat = "dd/MM/yyyy HH:mm"
         return formatter.stringFromDate(date)
+    }
+    
+    func parseDate(string: String) -> NSDate? {
+        let parser = NSDateFormatter()
+        parser.dateFormat = "dd/MM/yyyy HH:mm"
+        return parser.dateFromString(string)
     }
     
     override func didReceiveMemoryWarning() {
